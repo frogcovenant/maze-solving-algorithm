586a3318eef5d7474827195ca34e4dfd59bee593
Alejandra Judith Velázquez Ramírez <judithvelram@gmail.com>
2022-03-08 07:11:29 -0600

    Writes images at each step avoiding out of bounds

--- mano_derecha.py
+++ mano_derecha.py
@@ -1,220 +1,171 @@
 import numpy as np
 import cv2
+import os
+from PIL import Image
 
 maze = [
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
-[1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1],
-[1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,1],
-[1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1],
-[1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1],
-[1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1],
-[1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
-[1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1],
-[1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1],
-[1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1],
-[1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1],
-[1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1],
-[1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1],
-[1,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1],
-[1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1],
-[1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-[1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1],
-[1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1],
-[1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,1],
-[1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-[1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1],
-[1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1],
-[1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1],
-[1,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1],
-[1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1],
-[1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,1],
-[1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1],
-[1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1],
-[1,0,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1],
-[1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1],
-[1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1],
-[1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-[1,0,1,0,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,1],
-[1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,1,1,1,1],
-[1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1],
-[1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,1],
-[1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1],
-[1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1],
-[1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1],
-[1,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1],
-[1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0,1],
-[1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,1],
-[1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1],
-[1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1],
-[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+[1,.25,1,1,1],
+[1,0,0,0,1],
+[1,0,1,1,1],
+[1,0,0,0,1],
+[1,1,1,0,1],
 ]
 
+counter = 0
 maze_np = np.array(maze) * 255
 maze_np = 255 - maze_np
-cv2.imwrite("maze_images/maze.png", maze_np)
-
-pos = [49, 49] # posicion de inicio
+cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np) # laberinto vacio
+counter += 1
+pos = [4, 3] # posicion de inicio
 print(pos)
+maze_np[pos[0], pos[1]] = 128
+cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 facing = "up"
 visited= {} # añadir a visitados
-counter = 0
+
 while 1:
-    #print(f"pos actual: {pos[0]}, {pos[1]}\tfacing: {facing}")
-    #print(pos for pos in visited)
-    counter += 1
-    input()
-    maze_np[pos[0], pos[1]] = 128
-    cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
-    if maze[pos[0]][pos[1]] == '*': break # revisar que llego
+    try:
+        if maze[pos[0]][pos[1]] == 0.25: 
+            print("termino")
+            break # revisar que llego
 
-    
-    if facing == "up":
-        if maze[pos[0]][pos[1]+1] == 0: # si a tu derecha hay camino, debes girar
-            counter += 1
-            facing = "right"
-            pos[1]+=1
-            
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+        print(f"pos actual: {pos[0]}, {pos[1]}\t facing: {facing}")
+        #print(pos for pos in visited)
+        #input()
+        if pos[0] >= 0 and pos[0] < len(maze[0]):
+                if pos[1] >= 0 and pos[1] < len(maze[1]): # revisar que no se ha salido del arreglo
+                    if facing == "up":
+                        if maze[pos[0]][pos[1]+1] in [0, 0.25]: # si a tu derecha hay camino, debes girar
+                            counter += 1
+                            facing = "right"
+                            pos[1]+=1
+                            
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]-1][pos[1]] == 0: # sino, trata de seguir adelante
-            counter += 1
-            pos[0] -= 1
+                        elif maze[pos[0]-1][pos[1]] in [0, 0.25]: # sino, trata de seguir adelante
+                            counter += 1
+                            pos[0] -= 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]][pos[1]-1] == 0: # sino, revisa tu izquierda
-            counter += 1
-            facing = "left"
-            pos[1]-=1
+                        elif maze[pos[0]][pos[1]-1] in [0, 0.25]: # sino, revisa tu izquierda
+                            counter += 1
+                            facing = "left"
+                            pos[1]-=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        else: # sino, regresa
-            counter += 1
-            facing = "down"
-            pos[0] += 1
+                        else: # sino, regresa
+                            counter += 1
+                            facing = "down"
+                            pos[0] += 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-    if facing == "down":
-        if maze[pos[0]][pos[1]-1] == 0: # si a tu derecha hay camino, debes girar
-            counter += 1
-            facing = "left"
-            pos[1]-=1
+                    if facing == "down":
+                        if maze[pos[0]][pos[1]] == 2: break # revisar que llego
+                        if maze[pos[0]][pos[1]-1] in [0, 0.25]: # si a tu derecha hay camino, debes girar
+                            counter += 1
+                            facing = "left"
+                            pos[1]-=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]+1][pos[1]] == 0: # sino, trata de seguir adelante
-            counter += 1
-            pos[0] += 1
+                        elif maze[pos[0]+1][pos[1]] in [0, 0.25]: # sino, trata de seguir adelante
+                            counter += 1
+                            pos[0] += 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]][pos[1]+1] == 0: # sino, revisa tu izquierda
-            counter += 1
-            facing = "right"
-            pos[1]+=1
+                        elif maze[pos[0]][pos[1]+1] in [0, 0.25]: # sino, revisa tu izquierda
+                            counter += 1
+                            facing = "right"
+                            pos[1]+=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        else: # sino, regresa
-            counter += 1
-            facing = "up" 
-            pos[0] -= 1
+                        else: # sino, regresa
+                            counter += 1
+                            facing = "up" 
+                            pos[0] -= 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-    if facing == "left":
-        if maze[pos[0]-1][pos[1]] == 0: # si a tu derecha hay camino, debes girar
-            counter += 1
-            facing = "up"
-            pos[0]-=1
+                    if facing == "left":
+                        if maze[pos[0]][pos[1]] == 2: break # revisar que llego
+                        if maze[pos[0]-1][pos[1]] in [0, 0.25]: # si a tu derecha hay camino, debes girar
+                            counter += 1
+                            facing = "up"
+                            pos[0]-=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
-            
-        elif maze[pos[0]][pos[1]-1] == 0: # sino, trata de seguir adelante
-            counter += 1
-            pos[1] -= 1
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            
+                        elif maze[pos[0]][pos[1]-1] in [0, 0.25]: # sino, trata de seguir adelante
+                            counter += 1
+                            pos[1] -= 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]+1][pos[1]] == 0: # sino, revisa tu izquierda
-            counter += 1
-            facing = "down"
-            pos[0]+=1
+                        elif maze[pos[0]+1][pos[1]] in [0, 0.25]: # sino, revisa tu izquierda
+                            counter += 1
+                            facing = "down"
+                            pos[0]+=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
-        
-        else: # sino, regresa
-            counter += 1
-            facing = "right" 
-            pos[1] += 1
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                        
+                        else: # sino, regresa
+                            counter += 1
+                            facing = "right" 
+                            pos[1] += 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-    if facing == "right":
-        if maze[pos[0]+1][pos[1]] == 0: # si a tu derecha hay camino, debes girar
-            counter += 1
-            facing = "down"
-            pos[0]+=1
+                    if facing == "right":
+                        if maze[pos[0]][pos[1]] == 2: break # revisar que llego
+                        if maze[pos[0]+1][pos[1]] in [0, 0.25]: # si a tu derecha hay camino, debes girar
+                            counter += 1
+                            facing = "down"
+                            pos[0]+=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]][pos[1]+1] == 0: # sino, trata de seguir adelante
-            counter += 1
-            pos[1] += 1
+                        elif maze[pos[0]][pos[1]+1] in [0, 0.25]: # sino, trata de seguir adelante
+                            counter += 1
+                            pos[1] += 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        elif maze[pos[0]-1][pos[1]] == 0: # sino, revisa tu izquierda
-            counter += 1
-            facing = "up"
-            pos[0]-=1
+                        elif maze[pos[0]-1][pos[1]] in [0, 0.25]: # sino, revisa tu izquierda
+                            counter += 1
+                            facing = "up"
+                            pos[0]-=1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
 
-        else: # sino, regresa
-            counter += 1
-            facing = "left" 
-            pos[1] -= 1
+                        else: # sino, regresa
+                            counter += 1
+                            facing = "left" 
+                            pos[1] -= 1
 
-            input()
-            maze_np[pos[0], pos[1]] = 128
-            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+                            maze_np[pos[0], pos[1]] = 128
+                            cv2.imwrite("maze_images/maze"+str(counter)+".png", maze_np)
+
+    except:
+        continue
+        print("out of bonds")
 
